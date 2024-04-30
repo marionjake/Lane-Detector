7790b3bbb2afcae8b1c21b7b2b814b110e3c7e24
Marion Ajijala <marion.ajijala@airmconsulting.com>
2024-04-30 17:17:09 -0600

    feat: add Hough transformation

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 
 std::vector<cv::Point> getROIVertices(const cv::Mat& img)
@@ -36,6 +37,54 @@ cv::Mat regionOfInterest(const cv::Mat& img, const std::vector<cv::Point>& verti
     return masked_image;
 }
 
+cv::Mat getLineSegments(cv::Mat& img)
+{
+    cv::Mat linedImg;
+    cv::HoughLinesP(
+        img,
+        linedImg,
+        6,
+        M_PI/60,
+        160,
+        40.0,
+        25.0
+    );
+
+    if(linedImg.empty()) {
+        std::cerr << "Could not get line segments via Hough Transform\n";
+    }
+    return linedImg;
+}
+
+cv::Mat drawLines(cv::Mat& img, cv::Mat& linedImg, const cv::Scalar color, int thickness = 3)
+{
+    if(img.empty()) {
+        std::cerr << " Empty lined image!\n";
+        return cv::Mat{};
+    } 
+    if(img.empty()) {
+        std::cerr << "Empty base image!\n";
+        return cv::Mat{};
+    }
+
+    cv::Mat line_img(img.rows, img.cols, CV_8UC3, cv::Scalar(0, 0, 0));
+
+    // Loop over all lines and draw them on the blank image
+    for (int i = 0; i < linedImg.rows; ++i) {
+        cv::Vec4i line = linedImg.at<cv::Vec4i>(i);
+        int x1 = line[0];
+        int y1 = line[1];
+        int x2 = line[2];
+        int y2 = line[3];
+        cv::line(line_img, cv::Point(x1, y1), cv::Point(x2, y2), color, thickness);
+    }
+
+    // Merge the image with the lines onto the original.
+    cv::Mat lineDrawn;
+    cv::addWeighted(img, 0.8, line_img, 1.0, 0.0, lineDrawn);
+    return lineDrawn;
+}
+
 int main(int argc, char** argv)
 {
     // Read image 
@@ -50,9 +99,13 @@ int main(int argc, char** argv)
 
     // Get region of interest 
     auto ROIvertices = getROIVertices(img);
-    const auto croppedImage = regionOfInterest(imgCanny, ROIvertices);
+    auto croppedImage = regionOfInterest(imgCanny, ROIvertices);
 
-    cv::imshow(" Outline lanes ", croppedImage);
+    //get line segments via Hough Transform 
+    auto linedImg = getLineSegments(croppedImage);
+    auto lineDrawn = drawLines(img, linedImg, cv::Scalar(255, 0, 0));
+
+    cv::imshow(" Outline lanes ", lineDrawn);
     auto key = cv::waitKey(0);
 }
 
